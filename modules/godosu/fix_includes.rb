d1e250cc4a78afab044a4046abee80ed145f88ed
kobewi <kobewi4e@gmail.com>
2022-07-24 17:59:21 +0200

    kill src

--- modules/godosu/fix_includes.rb
+++ modules/godosu/fix_includes.rb
@@ -4,7 +4,7 @@ def fix(dir)
         if Dir.exist?(f)
             fix(f)
         else
-            if not f.end_with?(".c") and not f.end_with?(".h")
+            if not f.end_with?(".c") and not f.end_with?(".h") and not f.end_with?(".y")
                 next
             end
 
@@ -21,4 +21,32 @@ def fix(dir)
     end
 end
 
+def fix2(root, dir = nil)
+    if not dir
+        dir = root
+    end
+
+    for file in Dir.entries(dir) - [".", ".."]
+        f = dir + "/" + file
+        if Dir.exist?(f)
+            fix2(f)
+        else
+            if not f.end_with?(".c") and not f.end_with?(".h") and not f.end_with?(".y")
+                next
+            end
+
+            lines = File.readlines(f)
+            lines.each.with_index do |line, i|
+                if line.start_with?("#include \"mruby")
+                    lines[i] = +"../" * [dir.count("/") - 2, 0].max + line
+                end
+            end
+
+            ff = File.open(f, "w")
+            ff.puts(lines.join)
+        end
+    end
+end
+
 fix("modules/godosu")
+fix2("modules/godosu")
