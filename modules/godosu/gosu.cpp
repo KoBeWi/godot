181f8bc1a557f4e692b1a25cbd10476b03b67403
kobewi <kobewi4e@gmail.com>
2022-08-20 21:53:17 +0200

    progress

--- modules/godosu/gosu.cpp
+++ modules/godosu/gosu.cpp
@@ -1,11 +1,32 @@
 #include "gosu.h"
 
-#include "mruby/compile.h"
+#include "core/object/class_db.h"
 
-mrb_value ruby_method(mrb_state *mrb, mrb_value self)
-{
-    print_line("test");
+static Gosu *global_gosu;
+
+mrb_value ruby_method(mrb_state *mrb, mrb_value self) {
+    global_gosu->draw_sth();
     return mrb_nil_value();
+}
+
+void Gosu::draw_sth() {
+    print_line("draw");
+}
+
+String Gosu::get_file_source(const String &p_path) {
+    Ref<FileAccess> f = FileAccess::open(p_path, FileAccess::READ);
+    PackedStringArray lines = f->get_as_utf8_string().split("\n");
+    for (int i = 0; i < lines.size(); i++) {
+        const String &line = lines[i];
+        if (line.begins_with("require_relative")) {
+            int start = line.find("\"");
+            int end = line.find("\"", start + 1);
+
+            String next_file = line.substr(start + 1, end - start - 1);
+            lines.write[i] = get_file_source(next_file);
+        }
+    }
+    return String("\n").join(lines);
 }
 
 void Gosu::_notification(int p_what) {
@@ -15,24 +36,40 @@ void Gosu::_notification(int p_what) {
 
     switch (p_what) {
         case NOTIFICATION_READY: {
-            print_line("print_here");
+            // mrb_value obj = mrb_load_string(mrb, "def dupa;ruby_method;end");
 
-            mrb_state *mrb = mrb_open();
-
-            if (!mrb) {
-                return;
-            }
-
-            mrb_define_method(mrb, mrb->object_class, "ruby_method", ruby_method, MRB_ARGS_NONE());
-            mrb_value obj = mrb_load_string(mrb, "def dupa;ruby_method;end");
-            print_line("...");
-            // mrb_value obj = mrb_load_string(mrb, "puts \"hello world\"");
-            // mrb_value obj = mrb_load_string(mrb, "def test;File.new('test.txt', 'w');puts :meh;end");
+            String source = get_file_source(main_file_path);
+            mrb_value obj = mrb_load_string(mrb, source.utf8().get_data());
             mrb_funcall(mrb, obj, "dupa", 0);
-            // mrb_funcall_argv(mrb, obj, MRB_SYM(test), 1, &obj); // Calling ruby function from test.rb.
-            mrb_close(mrb);
-
-            print_line("but did it work");
         } break;
     }
+}
+
+void Gosu::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_main_file_path"), &Gosu::set_main_file_path);
+	ClassDB::bind_method(D_METHOD("get_main_file_path"), &Gosu::get_main_file_path);
+    ADD_PROPERTY(PropertyInfo(Variant::STRING, "main_file_path"), "set_main_file_path", "get_main_file_path");
+}
+
+Gosu::Gosu() {
+    if (Engine::get_singleton()->is_editor_hint()) {
+        return;
+    }
+
+    global_gosu = this;
+    mrb = mrb_open();
+
+    auto gosu_module = mrb_define_module(mrb, "Gosu");
+    // mrb_define_module_function(mrb, gosu_module, "ruby_method", &Gosu::draw_sth, MRB_ARGS_NONE());
+    mrb_define_method(mrb, mrb->object_class, "ruby_method", ruby_method, MRB_ARGS_NONE());
+
+    // mrb_funcall_id(
+}
+
+Gosu::~Gosu() {
+    if (Engine::get_singleton()->is_editor_hint()) {
+        return;
+    }
+
+    mrb_close(mrb);
 }
--- modules/godosu/gosu.h
+++ modules/godosu/gosu.h
@@ -2,12 +2,29 @@
 #define GOSU_H
 
 #include "scene/gui/control.h"
+#include "mruby/compile.h"
 
 class Gosu : public Control {
 	GDCLASS(Gosu, Control);
 
+	mrb_state *mrb = nullptr;
+	
+	String main_file_path;
+
+	String get_file_source(const String &p_path);
+
 protected:
+	static void _bind_methods();
 	void _notification(int p_what);
+
+public:
+	void set_main_file_path(const String &p_path) { main_file_path = p_path; }
+	String get_main_file_path() { return main_file_path; }
+	
+	void draw_sth();
+
+	Gosu();
+	~Gosu();
 };
 
 #endif // GOSU_H
