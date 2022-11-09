8ffd2d40332bdc63bd037b3e263d752ab413184c
kobewi <kobewi4e@gmail.com>
2022-11-09 17:16:55 +0100

    Improve appearance of inspector checkboxes

--- editor/editor_properties.cpp
+++ editor/editor_properties.cpp
@@ -736,6 +736,7 @@ void EditorPropertyCheck::_checkbox_pressed() {
 void EditorPropertyCheck::update_property() {
 	bool c = get_edited_object()->get(get_edited_property());
 	checkbox->set_pressed(c);
+	checkbox->set_text(c ? TTR("On") : TTR("Off"));
 	checkbox->set_disabled(is_read_only());
 }
 
@@ -744,7 +745,6 @@ void EditorPropertyCheck::_bind_methods() {
 
 EditorPropertyCheck::EditorPropertyCheck() {
 	checkbox = memnew(CheckBox);
-	checkbox->set_text(TTR("On"));
 	add_child(checkbox);
 	add_focusable(checkbox);
 	checkbox->connect("pressed", callable_mp(this, &EditorPropertyCheck::_checkbox_pressed));
