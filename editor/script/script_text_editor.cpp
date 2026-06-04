551371edb64bd2ed830653147167da3783b362e6
kobewi <kobewi4e@gmail.com>
2026-06-04 19:21:24 +0200

    Fix incorrect TTRN usage in the editor

--- editor/script/script_text_editor.cpp
+++ editor/script/script_text_editor.cpp
@@ -2022,15 +2022,15 @@ void ScriptTextEditor::_set_drop_info_text(const Dictionary &p_info) const {
 
 	if (type == "files" || type == "files_and_dirs" || type == "resource") {
 		Array files = p_info.get("files", Array());
-		text = TTRN("Drop file path.", "Drop file paths.", files.size()) +
-				"\n" + vformat(TTRN("Hold %s: Add const preload by %s.", "Hold %s: Add const preloads by %s.", files.size()), c, default_drop_option) +
-				"\n" + vformat(TTRN("Hold %s+Shift: Add const preload by %s.", "Hold %s+Shift: Add const preloads by %s.", files.size()), c, alternate_drop_option) +
-				"\n" + vformat(TTRN("Hold %s: Add @export var pointing to the resource.", "Hold %s: Add @export vars pointing to the resources.", files.size()), keycode_get_string((Key)KeyModifierMask::ALT));
+		text = TPL(files.size(), TTR("Drop file path."), TTR("Drop file paths.")) +
+				"\n" + vformat(TPL(files.size(), TTR("Hold %s: Add const preload by %s."), TTR("Hold %s: Add const preloads by %s.")), c, default_drop_option) +
+				"\n" + vformat(TPL(files.size(), TTR("Hold %s+Shift: Add const preload by %s."), TTR("Hold %s+Shift: Add const preloads by %s.")), c, alternate_drop_option) +
+				"\n" + vformat(TPL(files.size(), TTR("Hold Alt: Add @export var pointing to the resource."), TTR("Hold Alt: Add @export vars pointing to the resources.")), keycode_get_string((Key)KeyModifierMask::ALT));
 	} else if (type == "nodes") {
 		Array nodes = p_info["nodes"];
-		text = TTRN("Drop node path.", "Drop node paths.", nodes.size()) +
-				"\n" + vformat(TTRN("Hold %s: Add @onready var pointing to the node path.", "Hold %s: Add @onready vars pointing to the node paths.", nodes.size()), c) +
-				"\n" + vformat(TTRN("Hold %s: Add @export var pointing to the node.", "Hold %s: Add @export vars pointing to the nodes.", nodes.size()), keycode_get_string((Key)KeyModifierMask::ALT));
+		text = TPL(nodes.size(), TTR("Drop node path."), TTR("Drop node paths.")) +
+				"\n" + vformat(TPL(nodes.size(), TTR("Hold %s: Add @onready var pointing to the node path."), TTR("Hold %s: Add @onready vars pointing to the node paths.")), c) +
+				"\n" + vformat(TPL(nodes.size(), TTR("Hold Alt: Add @export var pointing to the node."), TTR("Hold Alt: Add @export vars pointing to the nodes.")), keycode_get_string((Key)KeyModifierMask::ALT));
 	} else if (type == "obj_property") {
 		text = TTR("Drop property path.");
 	}
